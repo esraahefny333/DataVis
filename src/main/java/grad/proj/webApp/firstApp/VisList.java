7d7b98458d128d98f0730ea4dfa971b82ec1b263
esraahefny333 <esraahefny3@hotmail.com>
2017-04-18 15:01:11 +0300

    b

--- src/main/java/grad/proj/webApp/firstApp/VisList.java
+++ src/main/java/grad/proj/webApp/firstApp/VisList.java
@@ -3,7 +3,10 @@ package grad.proj.webApp.firstApp;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -27,7 +30,7 @@ import grad.proj.webApp.firstApp.results.Url;
 
 @Path("/visList")
 public class VisList { 
-
+/*
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public ArrayList<VisData> chart() throws ClassNotFoundException, SQLException
@@ -47,4 +50,63 @@ public class VisList {
 	        
 		return extract.getMainVis();
 	}
+	
+	
+	*/
+	@POST
+	//@Path("/create")
+	//@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<VisData> chart(
+	        @FormParam("tableName") String tableName,
+	        @FormParam("xAxis") String xAxis,
+	        @FormParam("yAxis") String yAxis,
+	        @FormParam("aggregateFunction") String aggregateFunction,
+	        @FormParam("selectorOnDimension") String selectorOnDimension,
+	        @FormParam("SpecificAttribute") String SpecificAttribute,
+	        @FormParam("firstOperator") String firstOperator,
+	        @FormParam("firstSelector") String firstSelector,
+	        @FormParam("secondOperator") String secondOperator,
+	        @FormParam("secondSelector") String secondSelector
+	        ) throws ClassNotFoundException, SQLException {
+	
+		//System.out.println(firstname);
+		// VisBuilder vb = new VisBuilder();
+	        VisExtract extract = new VisExtract(tableName, xAxis, yAxis,
+	        		aggregateFunction, selectorOnDimension,
+	        		SpecificAttribute, firstOperator, firstSelector,
+	        		secondOperator, secondSelector
+	        );
+
+	    
+	      ArrayList<VisData> mainVisList = new ArrayList<VisData> ();
+	      ArrayList<VisData> recommendationList_bruteForce = new ArrayList<VisData> ();
+	      ArrayList<VisData> recommendationList_optimized=new ArrayList<VisData> ();
+	        
+	        
+	        mainVisList= extract.getMainVis();
+	        recommendationList_bruteForce=extract.getRecommendations_bruteForce();
+	        recommendationList_optimized=extract.getRecommendation_optimized();
+	        
+	    //----------------------------------------    
+	        Result result = new Result();
+			Url url = new Url();
+			url.setUrl("http://kokoko.com");
+		    Axis axis = new Axis();
+		    Cell cell = new Cell();
+ 			Column column = new Column();
+			
+			
+			Config config = new Config();
+			Encoding encoding = new Encoding();
+			Facet facet = new Facet();
+			Scale scale = new Scale();
+			Transform transform = new Transform();
+			Tuple tuple = new Tuple();
+			result.setData(url);
+			//return result;
+
+		  return recommendationList_bruteForce;
+	}
+	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,9 +1,52 @@
+<!DOCTYPE html>
 <html>
 <body>
-    <h2>Jersey RESTful Web Application!</h2>
-    <p><a href="rest/VisList">Jersey resource</a>
-    <p>Visit <a href="http://jersey.java.net">Project Jersey website</a>
-    for more information on Jersey!
-    
+
+<form action="rest/visList" method="post">
+tableName:<br>
+  <input type="text" name="tableName" value="Traffic_Violations" >
+  <br>
+  
+ xAxis:<br>
+  <input type="text" name="xAxis" value="Race" >
+  <br>
+  
+yAxis:<br>
+  <input type="text" name="yAxis" value="Accident" >
+  <br>
+  
+  aggregateFunction:<br>
+  <input type="text" name="aggregateFunction" value="Count" >
+  <br>
+  
+  selectorOnDimension:<br>
+  <input type="text" name="selectorOnDimension" value="'yes'">
+  <br>
+  
+  SpecificAttribute:<br>
+  <input type="text" name="SpecificAttribute" value="Gender">
+  <br>
+  
+firstOperator:<br>
+  <input type="text" name="firstOperator" value="=">
+  <br>
+  
+  firstSelector:<br>
+  <input type="text" name="firstSelector" value="'M'">
+  <br>
+  
+  secondOperator:<br>
+  <input type="text" name="secondOperator" value="=">
+  <br>
+  
+  secondSelector:<br>
+  <input type="text" name="secondSelector" value="'F'">
+  <br>
+  
+  <input type="submit" value="Submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+
 </body>
 </html>
